7f8ee1ee047366f26c69f403f997b0646268ae14
Enguerrand Arminjon <enguerrand.Arminjon@epitech.eu>
2018-06-11 09:47:42 +0200

    first

---
+++ include/zappy.h
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2018
+** student
+** File description:
+** 11/06/18
+*/
+#pragma once
---
+++ srcs/main.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** student
+** File description:
+** 11/06/18
+*/
+
+int main()
+{
+	return (0);
+}

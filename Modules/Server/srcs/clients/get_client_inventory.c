c1085f6b86e11642138e9738c1521fe967625e1d
robzz <robin94320@gmail.com>
2018-06-13 16:04:20 +0200

    fix inventory time

--- Modules/Server/srcs/clients/get_client_inventory.c
+++ Modules/Server/srcs/clients/get_client_inventory.c
@@ -11,15 +11,6 @@
 char *inventory(t_server *server, t_message *cmd)
 {
 	char *str;
-<<<<<<< HEAD
-	const t_get_type tab[7] = {{client->inventory.food, "food"},
-	{client->inventory.linemate, "linemate"},
-	{client->inventory.deraumere, "deraumere"},
-	{client->inventory.sibur, "sibur"},
-	{client->inventory.mendiane, "mendiane"},
-	{client->inventory.phiras, "phiras"},
-	{client->inventory.thystame, "thystame"}};
-=======
 	const t_get_type tab[7] = {{cmd->owner->inventory.food, "food"},
 	{cmd->owner->inventory.linemate, "linemate"},
 	{cmd->owner->inventory.deraumere, "deraumere"},
@@ -27,7 +18,6 @@ char *inventory(t_server *server, t_message *cmd)
 	{cmd->owner->inventory.mendiane, "mendiane"},
 	{cmd->owner->inventory.phiras, "phiras"},
 	{cmd->owner->inventory.thystame, "thystame"}};
->>>>>>> Server
 
 	cmd->finish_date = time_until_finish(INVENTORY_TIME, server->opts->freq);
 	asprintf(&str, "[");

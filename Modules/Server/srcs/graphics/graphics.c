9dfe471bffd9ba6b3270ed8e8624837e7b45a823
Benjamin GAYMAY <benjamin.gaymay@epitech.eu>
2018-06-19 11:34:14 +0200

    graphics teams

--- Modules/Server/srcs/graphics/graphics.c
+++ Modules/Server/srcs/graphics/graphics.c
@@ -33,5 +33,7 @@ char *is_graphical(t_server *server, t_message *cmd)
 	g_client = client_to_graphical(server, cmd->owner);
 	remove_client(server, cmd->owner, false);
 	dprintf(g_client->socket, "msz %d %d\n", server->opts->x, server->opts->y);
+	for (size_t i = 0 ; server->opts->teams[i] ; i++)
+		dprintf(g_client->socket, "tna %s\n", server->opts->teams[i]->name);
 	return (NULL);
 }

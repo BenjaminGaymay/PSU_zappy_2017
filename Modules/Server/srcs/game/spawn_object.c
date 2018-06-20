ab683fd10de3f57156cec65c66bc117ec8ad3eca
robzz <robin94320@gmail.com>
2018-06-20 11:26:36 +0200

    add macro spawn

--- Modules/Server/srcs/game/spawn_object.c
+++ Modules/Server/srcs/game/spawn_object.c
@@ -32,13 +32,20 @@ t_time *time_creator(t_server *server)
 	static t_time *timing;
 
 	timing = malloc(sizeof(t_time) * 7);
-	timing[0] = (t_time){time_until_finish(20, server->opts->freq), 20};
-	timing[1] = (t_time){time_until_finish(30, server->opts->freq), 30};
-	timing[2] = (t_time){time_until_finish(40, server->opts->freq), 40};
-	timing[3] = (t_time){time_until_finish(50, server->opts->freq), 50};
-	timing[4] = (t_time){time_until_finish(60, server->opts->freq), 60};
-	timing[5] = (t_time){time_until_finish(70, server->opts->freq), 70};
-	timing[6] = (t_time){time_until_finish(80, server->opts->freq), 80};
+	timing[0] = (t_time){time_until_finish(SPAWN_FOOD,
+		server->opts->freq), SPAWN_FOOD};
+	timing[1] = (t_time){time_until_finish(SPAWN_LINEMATE,
+		server->opts->freq), SPAWN_LINEMATE};
+	timing[2] = (t_time){time_until_finish(SPAWN_DERAUMERE,
+		server->opts->freq), SPAWN_DERAUMERE};
+	timing[3] = (t_time){time_until_finish(SPAWN_SIBUR,
+		server->opts->freq), SPAWN_SIBUR};
+	timing[4] = (t_time){time_until_finish(SPAWN_MENDIANE,
+		server->opts->freq), SPAWN_MENDIANE};
+	timing[5] = (t_time){time_until_finish(SPAWN_PHIRAS,
+		server->opts->freq), SPAWN_PHIRAS};
+	timing[6] = (t_time){time_until_finish(SPAWN_THYSTAME,
+		server->opts->freq), SPAWN_THYSTAME};
 	return (timing);
 }
 
